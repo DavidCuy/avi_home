f383fceb66facb95780fe9f55a5023b4fce50d9a
GuillermoSanchezLopez <guillermo.sanchez.lopez93@gmail.com>
2019-08-12 20:46:25 -0500

    Creacion de rama
    Se crea rama backend para proyecto final cinta negra

---
+++ avi_home_be/src/index.js
@@ -0,0 +1,40 @@
+//dotenv = require('dotenv').config(); //Libreria para acceder a las variables del .env
+const { GraphQLServer } = require('graphql-yoga');
+const { importSchema } = require('graphql-import');
+const { makeExecutableSchema } = require('graphql-tools');
+const typeDefs = importSchema('./src/schema.graphql');//Definimos constantes necesarias para GraphQL
+const mongoose = require('mongoose'); //Paquete para MongoDB connector
+
+/**
+ * Conexion a mondo-db
+ */
+mongoose.connect(process.env.MONGOURI,
+  {useNewUrlParser: true}, (err) => {
+      if(!err){
+          console.log('Conectado a Mongo exitosamente');
+      }
+      else{
+          console.log('Error al conectar: ' + err);
+      }
+  }
+);
+
+const resolvers = {
+  Query: {
+  },
+  Mutation: {
+  }
+};
+
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+//Variable constante para el server de GraphQL
+const server = new GraphQLServer({ 
+  schema,
+  context: async({request}) => request
+})
+server.start(() => console.log('Server is running on localhost:4000'))
